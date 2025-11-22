7203cf4f0290f34c524aa2c774abc07a13523b8a
Phenomenal07 <maharjansaugat07@gmail.com>
2025-11-22 20:25:26 +0545

    deleted exe files and uplated answer

--- src/q28.c
+++ src/q28.c
@@ -1,1 +1,27 @@
 // Write a recursive function named calculateBinary that takes an integer n as input and returns its binary representation as a string.
+#include <stdio.h>
+
+void calculateBinary(int n) {
+    if (n == 0) {
+        return; 
+    } else {
+        calculateBinary(n / 2);
+        printf("%d", n % 2);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num); 
+
+    printf("The binary representation of %d is: ", num);
+    if (num == 0) {
+        printf("0"); 
+    } else {
+        calculateBinary(num);
+    }
+    printf("\n");
+
+    return 0;
+}
